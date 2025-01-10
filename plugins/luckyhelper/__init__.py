6f0b44ebdb50ea8a89cee0b224b8ee5e0399642e
KoWming <1075151242@qq.com>
2025-01-10 13:33:16 +0800

    Update __init__.py

--- plugins/luckyhelper/__init__.py
+++ plugins/luckyhelper/__init__.py
@@ -1,20 +1,20 @@
-from datetime import datetime, timedelta
-
-from typing import Optional, Any, List, Dict, Tuple
+import glob
+import os
+import shutil
 import time
+from datetime import datetime, timedelta
+from pathlib import Path
+
 import pytz
-import jwt
-import requests
-from requests import Session, Response
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
-from app.core.event import eventmanager, Event
 
+from app import schemas
 from app.core.config import settings
-from app.log import logger
 from app.plugins import _PluginBase
-from app.schemas.types import EventType, NotificationType
-from app.utils.http import RequestUtils
+from typing import Any, List, Dict, Tuple, Optional
+from app.log import logger
+from app.schemas import NotificationType
 
 
 class LuckyHelper(_PluginBase):
@@ -39,127 +39,192 @@ class LuckyHelper(_PluginBase):
 
     # 私有属性
     _enabled = False
+    # 任务执行间隔
+    _cron = None
+    _cnt = None
     _onlyonce = False
+    _notify = False
 
-    # 备份
-    _backup_cron = None
-    _backups_notify = False
-    _host = None
-    _openToken = None
+    # 定时器
     _scheduler: Optional[BackgroundScheduler] = None
 
     def init_plugin(self, config: dict = None):
         # 停止现有任务
         self.stop_service()
+
         if config:
-            self._backup_cron = config.get("backupcron")
-            self._backups_notify = config.get("backupsnotify")
-            self._host = config.get("host")
-            self._openToken = config.get("openToken")
+            self._enabled = config.get("enabled")
+            self._cron = config.get("cron")
+            self._cnt = config.get("cnt")
+            self._notify = config.get("notify")
+            self._onlyonce = config.get("onlyonce")
 
             # 加载模块
-            if self._enabled or self._onlyonce:
-                # 定时服务
-                self._scheduler = BackgroundScheduler(timezone=settings.TZ)
-                # 立即运行一次
-                if self._onlyonce:
-                    logger.info(f"Lucky助手服务启动，立即运行一次")
-                    if self._backup_cron:
-                        self._scheduler.add_job(self.backup, 'date',
-                                                run_date=datetime.now(
-                                                    tz=pytz.timezone(settings.TZ)) + timedelta(seconds=3),
-                                                name="Lucky助手-备份")
-                    # 关闭一次性开关
-                    self._onlyonce = False
-                    # 保存配置
-                    self.__update_config()
-                # 周期运行
-                if self._backup_cron:
-                    try:
-                        self._scheduler.add_job(func=self.backup,
-                                                trigger=CronTrigger.from_crontab(self._backup_cron),
-                                                name="Lucky助手-备份")
-                    except Exception as err:
-                        logger.error(f"定时任务配置错误：{str(err)}")
-                        # 推送实时消息
-                        self.systemmessage.put(f"执行周期配置错误：{err}")
-                # 启动任务
-                if self._scheduler.get_jobs():
-                    self._scheduler.print_jobs()
-                    self._scheduler.start()
+        if self._onlyonce:
+            self._scheduler = BackgroundScheduler(timezone=settings.TZ)
+            logger.info(f"自动备份服务启动，立即运行一次")
+            self._scheduler.add_job(func=self.__backup, trigger='date',
+                                    run_date=datetime.now(tz=pytz.timezone(settings.TZ)) + timedelta(seconds=3),
+                                    name="自动备份")
+            # 关闭一次性开关
+            self._onlyonce = False
+            self.update_config({
+                "onlyonce": False,
+                "cron": self._cron,
+                "enabled": self._enabled,
+                "cnt": self._cnt,
+                "notify": self._notify,
+            })
+
+            # 启动任务
+            if self._scheduler.get_jobs():
+                self._scheduler.print_jobs()
+                self._scheduler.start()
+
+    def api_backup(self, apikey: str):
+        """
+        API调用备份
+        """
+        if apikey != settings.API_TOKEN:
+            return schemas.Response(success=False, message="API密钥错误")
+        return self.__backup()
+
+    def __backup(self):
+        """
+        自动备份、删除备份
+        """
+        logger.info(f"当前时间 {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))} 开始备份")
+
+        # docker用默认路径
+        bk_path = self.get_data_path()
+
+        # 备份
+        zip_file = self.backup_file(bk_path=bk_path)
+
+        if zip_file:
+            success = True
+            msg = f"备份完成 备份文件 {zip_file}"
+            logger.info(msg)
+        else:
+            success = False
+            msg = "创建备份失败"
+            logger.error(msg)
+
+        # 清理备份
+        bk_cnt = 0
+        del_cnt = 0
+        if self._cnt:
+            # 获取指定路径下所有以"bk"开头的文件，按照创建时间从旧到新排序
+            files = sorted(glob.glob(f"{bk_path}/bk**"), key=os.path.getctime)
+            bk_cnt = len(files)
+            # 计算需要删除的文件数
+            del_cnt = bk_cnt - int(self._cnt)
+            if del_cnt > 0:
+                logger.info(
+                    f"获取到 {bk_path} 路径下备份文件数量 {bk_cnt} 保留数量 {int(self._cnt)} 需要删除备份文件数量 {del_cnt}")
+
+                # 遍历并删除最旧的几个备份
+                for i in range(del_cnt):
+                    os.remove(files[i])
+                    logger.debug(f"删除备份文件 {files[i]} 成功")
+            else:
+                logger.info(
+                    f"获取到 {bk_path} 路径下备份文件数量 {bk_cnt} 保留数量 {int(self._cnt)} 无需删除")
+
+        # 发送通知
+        if self._notify:
+            self.post_message(
+                mtype=NotificationType.SiteMessage,
+                title="【自动备份任务完成】",
+                text=f"创建备份{'成功' if zip_file else '失败'}\n"
+                     f"清理备份数量 {del_cnt}\n"
+                     f"剩余备份数量 {bk_cnt - del_cnt}")
+
+        return success, msg
+
+    @staticmethod
+    def backup_file(bk_path: Path = None):
+        """
+        @param bk_path     自定义备份路径
+        """
+        try:
+            # 创建备份文件夹
+            config_path = Path(settings.CONFIG_PATH)
+            backup_file = f"bk_{time.strftime('%Y%m%d%H%M%S')}"
+            backup_path = bk_path / backup_file
+            backup_path.mkdir(parents=True)
+
+            # 把现有的相关文件进行copy备份
+            category_file = config_path / "category.yaml"
+            if category_file.exists():
+                shutil.copy(category_file, backup_path)
+            userdb_file = config_path / "user.db"
+            if userdb_file.exists():
+                shutil.copy(userdb_file, backup_path)
+
+            zip_file = str(backup_path) + '.zip'
+            if os.path.exists(zip_file):
+                zip_file = str(backup_path) + '.zip'
+            shutil.make_archive(str(backup_path), 'zip', str(backup_path))
+            shutil.rmtree(str(backup_path))
+            return zip_file
+        except IOError:
+            return None
 
     def get_state(self) -> bool:
         return self._enabled
-
-    def backup(self):
-        """
-        备份
-        """
-        try:
-            logger.info(f"Lucky-备份-准备执行")
-            backup_url = '%s/api/configure?openToken=%s' % (self._host, self._openToken)
-            result = (RequestUtils(headers={"Authorization": self.get_jwt()})
-                    .get_res(backup_url))
-            
-            # 检查响应状态码
-            if result.status_code == 200:
-                # 假设响应内容是备份文件的二进制数据
-                backup_data = result.content
-                
-                # 定义保存文件的路径
-                backup_file_path = f"config/plugins/LuckyHelper/"
-                
-                # 保存文件到本地
-                with open(backup_file_path, 'wb') as backup_file:
-                    backup_file.write(backup_data)
-                
-                logger.info(f"Lucky-备份完成，文件保存到 {backup_file_path}")
-                
-                if self._backups_notify:
-                    self.post_message(
-                        mtype=NotificationType.Plugin,
-                        title="【Lucky助手-备份成功】",
-                        text=f"备份成功！文件保存到 {backup_file_path}")
-            else:
-                if self._backups_notify:
-                    self.post_message(
-                        mtype=NotificationType.Plugin,
-                        title="【Lucky助手-备份失败】",
-                        text=f"备份失败！\n【失败原因】:{result.json().get('msg', '未知错误')}")
-                logger.error(f"Lucky-备份失败 Error code: {result.status_code}, message: {result.json().get('msg', '未知错误')}")
-        except Exception as e:
-            logger.error(f"Lucky-备份失败,网络异常,请检查Lucky服务是否正常: {str(e)}")
-            return []
-
-    @eventmanager.register(EventType.PluginAction)
-    def remote_sync(self, event: Event):
-        pass
 
     @staticmethod
     def get_command() -> List[Dict[str, Any]]:
         pass
 
     def get_api(self) -> List[Dict[str, Any]]:
-        pass 
+        return [{
+            "path": "/backup",
+            "endpoint": self.api_backup,
+            "methods": ["GET"],
+            "summary": "MoviePilot备份",
+            "description": "MoviePilot备份",
+        }]
 
-    def get_jwt(self) -> str:
-        # 减少接口请求直接使用jwt
-        payload = {
-            "exp": int(time.time()) + 28 * 24 * 60 * 60,
-            "iat": int(time.time())
-        }
-        encoded_jwt = jwt.encode(payload, self._secretKey, algorithm="HS256")
-        logger.debug(f"LuckyHelper get jwt---》{encoded_jwt}")
-        return "Bearer "+encoded_jwt
+    def get_service(self) -> List[Dict[str, Any]]:
+        """
+        注册插件公共服务
+        [{
+            "id": "服务ID",
+            "name": "服务名称",
+            "trigger": "触发器：cron/interval/date/CronTrigger.from_crontab()",
+            "func": self.xxx,
+            "kwargs": {} # 定时器参数
+        }]
+        """
+        if self._enabled and self._cron:
+            return [{
+                "id": "AutoBackup",
+                "name": "自动备份定时服务",
+                "trigger": CronTrigger.from_crontab(self._cron),
+                "func": self.__backup,
+                "kwargs": {}
+            }]
+
+    def backup(self) -> schemas.Response:
+        """
+        API调用备份
+        """
+        success, msg = self.__backup()
+        return schemas.Response(
+            success=success,
+            message=msg
+        )
 
     def get_form(self) -> Tuple[List[dict], Dict[str, Any]]:
         """
-        拼装插件配置页面，需要返回两块数据： 1、页面配置；2、数据结构
+        拼装插件配置页面，需要返回两块数据：1、页面配置；2、数据结构
         """
         return [
             {
-                "component": "VForm",
-                "content": [
+                'component': 'VForm',
+                'content': [
                     {
                         'component': 'VRow',
                         'content': [
@@ -189,8 +254,8 @@ class LuckyHelper(_PluginBase):
                                     {
                                         'component': 'VSwitch',
                                         'props': {
-                                            'model': 'backupsnotify',
-                                            'label': '备份通知',
+                                            'model': 'notify',
+                                            'label': '开启通知',
                                         }
                                     }
                                 ]
@@ -252,6 +317,11 @@ class LuckyHelper(_PluginBase):
                                     }
                                 ]
                             },
+                        ]
+                    },
+                    {
+                        'component': 'VRow',
+                        'content': [
                             {
                                 'component': 'VCol',
                                 'props': {
@@ -262,9 +332,9 @@ class LuckyHelper(_PluginBase):
                                     {
                                         'component': 'VTextField',
                                         'props': {
-                                            'model': 'cnt',
-                                            'label': '保留份数',
-                                            'hint': '最大保留备份数',
+                                            'model': 'cron',
+                                            'label': '备份周期',
+                                            'hint': '输入5位cron表达式',
                                             'persistent-hint': True
                                         }
                                     }
@@ -280,10 +350,9 @@ class LuckyHelper(_PluginBase):
                                     {
                                         'component': 'VTextField',
                                         'props': {
-                                            'model': 'backupcron',
-                                            'label': '自动备份',
-                                            'placeholder': '0 7 * * *',
-                                            'hint': '输入5位cron表达式',
+                                            'model': 'cnt',
+                                            'label': '保留份数',
+                                            'hint': '最大保留备份数',
                                             'persistent-hint': True
                                         }
                                     }
@@ -316,9 +385,9 @@ class LuckyHelper(_PluginBase):
             }
         ], {
             "enabled": False,
-            "onlyonce": False,
-            "backupsnotify": False,
-            }
+            "request_method": "POST",
+            "webhook_url": ""
+        }
 
     def get_page(self) -> List[dict]:
         pass
@@ -335,4 +404,3 @@ class LuckyHelper(_PluginBase):
                 self._scheduler = None
         except Exception as e:
             logger.error("退出插件失败：%s" % str(e))
-
