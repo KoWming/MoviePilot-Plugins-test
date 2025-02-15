ae12fd210e0d124bf783b3c6bb132b29bd4feb58
KoWming <1075151242@qq.com>
2025-02-15 17:58:28 +0800

    up

--- plugins/stationcall/__init__.py
+++ plugins/stationcall/__init__.py
@@ -25,7 +25,7 @@ class StationCall(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/KoWming/MoviePilot-Plugins/main/icons/Lucky_B.png"
     # 插件版本
-    plugin_version = "0.5.1"
+    plugin_version = "0.5.2"
     # 插件作者
     plugin_author = "KoWming"
     # 作者主页
