043623c02b2107a5701e9c89089449e3fcd4edeb
KoWming <1075151242@qq.com>
2025-02-22 16:16:09 +0800

    Update __init__.py

--- plugins/sitechatroom/__init__.py
+++ plugins/sitechatroom/__init__.py
@@ -426,9 +426,13 @@ class SiteChatRoom(_PluginBase):
         """
         自动向站点发送消息
         """
-        if self._chat_sites:
-            site_msgs = self.parse_site_messages(self._sites_messages)
-            self.__send_msgs(do_sites=self._chat_sites, site_msgs=site_msgs, event=event)
+        try:
+            if self._chat_sites:
+                site_msgs = self.parse_site_messages(self._sites_messages)
+                self.__send_msgs(do_sites=self._chat_sites, site_msgs=site_msgs, event=event)
+            logger.info("send_site_messages 函数执行成功")
+        except Exception as e:
+            logger.error(f"send_site_messages 函数执行失败: {str(e)}")
 
     def __send_msgs(self, do_sites: list, site_msgs: Dict[str, List[str]]):
         """
