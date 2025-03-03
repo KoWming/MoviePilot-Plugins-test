b1743f54f6648052ef49bebaa73bcd29caf1e540
KoWming <1075151242@qq.com>
2025-03-03 21:13:39 +0800

    Update __init__.py

--- plugins/msgnotify/__init__.py
+++ plugins/msgnotify/__init__.py
@@ -49,6 +49,7 @@ class MsgNotify(_PluginBase):
         if apikey != settings.API_TOKEN:
             return schemas.Response(success=False, message="API令牌错误!")
         
+        apikey = request.apikey
         title = request.title
         text = request.text
         logger.info(f"收到以下消息:\n{title}\n{text}")
